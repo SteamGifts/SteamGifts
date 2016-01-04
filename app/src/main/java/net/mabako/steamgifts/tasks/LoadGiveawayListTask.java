6b821c24328362d18447968e7f8154088613dc68
Marcus Bauer <mabako@gmail.com>
2016-01-04 21:24:07 +0100

    fixed an issue with items without a proper app ID
    One example would, of course, be the Humble Indie Bundle, which has no store links

--- app/src/main/java/net/mabako/steamgifts/tasks/LoadGiveawayListTask.java
+++ app/src/main/java/net/mabako/steamgifts/tasks/LoadGiveawayListTask.java
@@ -41,10 +41,10 @@ public class LoadGiveawayListTask extends AsyncTask<Void, Void, List<Giveaway>> 
             Connection jsoup = Jsoup.connect("http://www.steamgifts.com/giveaways/search");
             jsoup.data("page", Integer.toString(page));
 
-            if(searchQuery != null)
+            if (searchQuery != null)
                 jsoup.data("q", searchQuery);
 
-            if(type != GiveawayListFragment.Type.ALL)
+            if (type != GiveawayListFragment.Type.ALL)
                 jsoup.data("type", type.name().toLowerCase());
 
             if (WebUserData.getCurrent().isLoggedIn())
@@ -70,9 +70,13 @@ public class LoadGiveawayListTask extends AsyncTask<Void, Void, List<Giveaway>> 
                 String giveawayLink = link.attr("href").substring(10, 15);
                 String giveawayName = link.attr("href").substring(16);
 
-                String iconSplit = icon.attr("href");
-                int gameId = iconSplit == null || iconSplit.length() < 5 ? -1 : Integer.parseInt(iconSplit.split("/")[4]);
-                Giveaway.Type type = "app".equals(iconSplit != null && iconSplit.length() >= 4 ? iconSplit.split("/")[3] : "") ? Giveaway.Type.APP : Giveaway.Type.SUB;
+                int gameId = -1;
+                Giveaway.Type type = Giveaway.Type.APP;
+                if (icon != link) {
+                    String[] iconSplit = icon.attr("href").split("/");
+                    gameId = Integer.parseInt(iconSplit[4]);
+                    type = "app".equals(iconSplit[3]) ? Giveaway.Type.APP : Giveaway.Type.SUB;
+                }
 
                 // Entries & Comments
                 Elements links = element.select(".giveaway__links a span");
