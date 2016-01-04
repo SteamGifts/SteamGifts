be1b41fc96afdef05573037cfd8f75dcf43a8082
Marcus Bauer <mabako@gmail.com>
2016-01-04 21:37:36 +0100

    vaguely working list handling
    Code is taken from https://gist.github.com/mlakkadshaw/5983704

--- app/src/main/java/net/mabako/steamgifts/adapters/viewholder/CustomHtmlTagHandler.java
+++ app/src/main/java/net/mabako/steamgifts/adapters/viewholder/CustomHtmlTagHandler.java
@@ -3,15 +3,34 @@ package net.mabako.steamgifts.adapters.viewholder;
 import android.text.Editable;
 import android.text.Html;
 import android.text.Spannable;
+import android.text.style.BulletSpan;
+import android.text.style.LeadingMarginSpan;
 import android.text.style.StrikethroughSpan;
 
 import org.xml.sax.XMLReader;
 
+import java.util.Vector;
+
 public class CustomHtmlTagHandler implements Html.TagHandler {
+    private int mListItemCount = 0;
+    private Vector<String> mListParents = new Vector<String>();
+
     @Override
     public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
-        if (tag.equalsIgnoreCase("del"))
+        if (tag.equalsIgnoreCase("del")) {
             processStrike(opening, output);
+        } else if (tag.equals("ul") || tag.equals("ol")) {
+            if (opening) {
+                mListParents.add(tag);
+            } else {
+                mListParents.remove(tag);
+            }
+
+            mListItemCount = 0;
+        } else if (tag.equals("li") && !opening) {
+            handleListTag(output);
+        }
+
     }
 
     private void processStrike(boolean opening, Editable output) {
@@ -42,6 +61,27 @@ public class CustomHtmlTagHandler implements Html.TagHandler {
                 }
             }
             return null;
+        }
+    }
+
+    private void handleListTag(Editable output) {
+        if (mListParents.lastElement().equals("ul")) {
+            output.append("\n");
+            String[] split = output.toString().split("\n");
+
+            int lastIndex = split.length - 1;
+            int start = output.length() - split[lastIndex].length() - 1;
+            output.setSpan(new BulletSpan(15 * mListParents.size()), start, output.length(), 0);
+        } else if (mListParents.lastElement().equals("ol")) {
+            mListItemCount++;
+
+            output.append("\n");
+            String[] split = output.toString().split("\n");
+
+            int lastIndex = split.length - 1;
+            int start = output.length() - split[lastIndex].length() - 1;
+            output.insert(start, mListItemCount + ". ");
+            output.setSpan(new LeadingMarginSpan.Standard(15 * mListParents.size()), start, output.length(), 0);
         }
     }
 }
