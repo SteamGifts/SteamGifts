dc1ff78d38ec2f41f02cc04c2800c378906a2436
Marcus Bauer <mabako@gmail.com>
2016-01-05 15:23:20 +0100

    slightly less wonky list handling

--- app/src/main/java/net/mabako/steamgifts/adapters/viewholder/Utils.java
+++ app/src/main/java/net/mabako/steamgifts/adapters/viewholder/Utils.java
@@ -2,6 +2,7 @@ package net.mabako.steamgifts.adapters.viewholder;
 
 import android.text.Html;
 import android.text.Spanned;
+import android.util.Log;
 
 public final class Utils {
     public static CharSequence fromHtml(String source) {
@@ -9,8 +10,9 @@ public final class Utils {
             CharSequence cs = Html.fromHtml(source, null, new CustomHtmlTagHandler());
             cs = cs.subSequence(0, cs.length() - 2);
             return cs;
-
         } catch (Exception e) {
+            Log.e(Utils.class.getSimpleName(), "Failed to parse HTML with custom parser", e);
+
             CharSequence cs = Html.fromHtml(source);
             cs = cs.subSequence(0, cs.length() - 2);
             return cs;
