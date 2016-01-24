c6f8a26c6a459efc8fee94fd5423c8d3fc29cbc0
Marcus Bauer <mabako@gmail.com>
2016-01-24 03:25:51 +0100

    loading/saving values from the filter

--- src/main/java/net/mabako/steamgifts/fragments/FilterGiveawayDialogFragment.java
+++ src/main/java/net/mabako/steamgifts/fragments/FilterGiveawayDialogFragment.java
@@ -6,26 +6,90 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.EditText;
 
 import net.mabako.steamgifts.R;
+import net.mabako.steamgifts.persistentdata.FilterData;
 
-public class FilterGiveawayDialogFragment extends DialogFragment {
+public class FilterGiveawayDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
+    private boolean requireReload = false;
+
+    private View view;
+    private FilterUpdatedListener listener;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity())
-                .setView(getActivity().getLayoutInflater().inflate(R.layout.fragment_filter, null, false))
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        FilterData f = FilterData.getCurrent();
+        view = getActivity().getLayoutInflater().inflate(R.layout.fragment_filter, null, false);
 
-                    }
-                })
+        AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setPositiveButton(R.string.ok, this)
                 .setNegativeButton(R.string.reset, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        // Clear all filter settings
+                        FilterData.clear();
 
+                        if (listener != null)
+                            listener.onFilterUpdated();
                     }
                 }).create();
+
+        setValue(view.findViewById(R.id.filter_entries_max), f.getMaxEntries());
+        setValue(view.findViewById(R.id.filter_level_max), f.getMaxLevel());
+        setValue(view.findViewById(R.id.filter_points_max), f.getMaxPoints());
+
+        setValue(view.findViewById(R.id.filter_entries_min), f.getMinEntries());
+        setValue(view.findViewById(R.id.filter_level_min), f.getMinLevel());
+        setValue(view.findViewById(R.id.filter_points_min), f.getMinPoints());
+
+        return dialog;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        FilterData oldFilterData = FilterData.getCurrent();
+        FilterData.clear();
+        FilterData newFilterData = FilterData.getCurrent();
+
+        newFilterData.setMaxEntries(getValueAndUpdateFlags(R.id.filter_entries_max, oldFilterData.getMaxEntries()));
+        newFilterData.setMaxLevel(getValueAndUpdateFlags(R.id.filter_level_max, oldFilterData.getMaxLevel()));
+        newFilterData.setMaxPoints(getValueAndUpdateFlags(R.id.filter_points_max, oldFilterData.getMaxPoints()));
+        newFilterData.setMinEntries(getValueAndUpdateFlags(R.id.filter_entries_min, oldFilterData.getMinEntries()));
+        newFilterData.setMinLevel(getValueAndUpdateFlags(R.id.filter_level_min, oldFilterData.getMinLevel()));
+        newFilterData.setMinPoints(getValueAndUpdateFlags(R.id.filter_points_min, oldFilterData.getMinPoints()));
+
+        if (listener != null && requireReload)
+            listener.onFilterUpdated();
+    }
+
+    private void setValue(View view, int value) {
+        if (value >= 0 && view instanceof EditText) {
+            EditText editText = (EditText) view;
+            editText.setText(String.valueOf(value));
+        }
+    }
+
+    private int getValueAndUpdateFlags(int editResource, int oldValue) {
+        EditText editText = (EditText) view.findViewById(editResource);
+
+        int newValue;
+        try {
+            newValue = Integer.parseInt(editText.getText().toString());
+        } catch (NumberFormatException e) {
+            newValue = -1;
+        }
+
+        if (oldValue != newValue)
+            requireReload = true;
+
+        return newValue;
+    }
+
+    public interface FilterUpdatedListener {
+        void onFilterUpdated();
     }
 }
---
+++ src/main/java/net/mabako/steamgifts/persistentdata/FilterData.java
@@ -0,0 +1,66 @@
+package net.mabako.steamgifts.persistentdata;
+
+/**
+ * Created by mabako on 24.01.2016.
+ */
+public class FilterData {
+    private static FilterData current = new FilterData();
+
+    private int minEntries = -1, maxEntries = -1, minPoints = -1, maxPoints = -1, minLevel = -1, maxLevel = -1;
+
+    public static FilterData getCurrent() {
+        return current;
+    }
+
+    public static void clear() {
+        current = new FilterData();
+    }
+
+    public int getMaxEntries() {
+        return maxEntries;
+    }
+
+    public int getMaxLevel() {
+        return maxLevel;
+    }
+
+    public int getMaxPoints() {
+        return maxPoints;
+    }
+
+    public int getMinEntries() {
+        return minEntries;
+    }
+
+    public int getMinLevel() {
+        return minLevel;
+    }
+
+    public int getMinPoints() {
+        return minPoints;
+    }
+
+    public void setMaxEntries(int maxEntries) {
+        this.maxEntries = maxEntries;
+    }
+
+    public void setMaxLevel(int maxLevel) {
+        this.maxLevel = maxLevel;
+    }
+
+    public void setMaxPoints(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public void setMinEntries(int minEntries) {
+        this.minEntries = minEntries;
+    }
+
+    public void setMinLevel(int minLevel) {
+        this.minLevel = minLevel;
+    }
+
+    public void setMinPoints(int minPoints) {
+        this.minPoints = minPoints;
+    }
+}
