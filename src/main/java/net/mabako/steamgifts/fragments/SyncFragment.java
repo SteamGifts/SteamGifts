0949201d7e7a333e48c8d47ec07100b5bf654940
Marcus Bauer <mabako@gmail.com>
2016-01-25 00:36:03 +0100

    simple sync activity template

---
+++ src/main/java/net/mabako/steamgifts/fragments/SyncFragment.java
@@ -0,0 +1,34 @@
+package net.mabako.steamgifts.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.mabako.steamgifts.R;
+import net.mabako.steamgifts.activities.WebViewActivity;
+
+public class SyncFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sync, container, false);
+
+
+        view.findViewById(R.id.privacy_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), WebViewActivity.class);
+                intent.putExtra(WebViewActivity.ARG_URL, "http://steamcommunity.com/my/edit/settings");
+
+                getActivity().startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
