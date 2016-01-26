4d681d22e90bc6cc24e154700651e9f49358510c
Marcus Bauer <mabako@gmail.com>
2016-01-26 04:16:40 +0100

    use fabric for crash reporting

--- app/src/main/java/net/mabako/steamgifts/Application.java
+++ app/src/main/java/net/mabako/steamgifts/Application.java
@@ -1,4 +1,9 @@
 package net.mabako.steamgifts;
 
 public class Application extends ApplicationTemplate {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupFabric(BuildConfig.FLAVOR);
+    }
 }
